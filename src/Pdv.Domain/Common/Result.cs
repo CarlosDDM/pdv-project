8bc59f7dba2e489c3a3f5b5b30e8adfbb742a077
Carlos Daniel <carlosdanieldiasmendes@gmail.com>
2026-08-28 13:07:56 -0300

    Feita a criação da classe de result pattern que vai padronizar os erros e respostas.

---
+++ src/Pdv.Domain/Common/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace Pdv.Domain.Common;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public string Error {  get; } = string.Empty;
+
+    protected Result(bool success, string error)
+    {
+        IsSuccess = success;
+        Error = error;  
+    }
+
+    public static Result Ok() => new(true, string.Empty);
+    public static Result Fail(string error) => new(false, error);
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(bool success, T? value, string error) : base(success, error)
+    {
+        Value = value;
+    }
+
+    public static Result<T> Ok(T value) => new(true, value, string.Empty);
+    public static new Result<T> Fail(string error) => new(false, default, error);
+}
